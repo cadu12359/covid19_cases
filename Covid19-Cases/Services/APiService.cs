b31f065b6b60bbba3f54549e2ca7e28a3a46fd73
cadu12359 <csatoslima03@gmail.com>
2020-04-08 17:27:52 -0300

    Project initical

---
+++ Covid19-Cases/Services/APiService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19_Cases.Services
+{
+    public class APiService
+    {
+        
+
+
+        
+    }
+}
---
+++ Covid19-Cases/ViewModels/HomeViewModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+using Covid19_Cases.Models;
+using Covid19_Cases.Views;
+
+namespace Covid19_Cases.ViewModels
+{
+    public class HomeViewModel : BaseViewModel
+    {
+        public ObservableCollection<Item> Items { get; set; }
+        public Command LoadItemsCommand { get; set; }
+
+        public HomeViewModel()
+        {
+            Title = "Casos Corona Vírus";
+            Items = new ObservableCollection<Item>();
+            //LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
+        }
+
+
+
+
+
+
+
+        /*
+        async Task ExecuteLoadItemsCommand()
+        {
+            IsBusy = true;
+
+            try
+            {
+                Items.Clear();
+                var items = await DataStore.GetItemsAsync(true);
+                foreach (var item in items)
+                {
+                    Items.Add(item);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+        */
+    }
+}
